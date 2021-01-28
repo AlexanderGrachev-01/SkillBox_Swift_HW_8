5acbc2567676a3bf4ab594eea1371fc730daf8ce
Alexander Grachev <s89519011368@yandex.ru>
2021-01-28 17:40:45 +0300

    Add images an Next Button work

--- HW_8/ViewController.swift
+++ HW_8/ViewController.swift
@@ -8,7 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var imageList = ["sneakers_1.png", "sneakers_2.png", "sneakers_3.png", "sneakers_4.png", "sneakers_5.png", "sneakers_6.png", "sneakers_7.png", "sneakers_8.png", "sneakers_9.png", "sneakers_10.png"]
+    var counter = 0
+    
+    @IBOutlet weak var sneakersImageView: UIImageView!
+    
+    
+    @IBAction func backButton(_ sender: Any) {
+        counter -= 1
+        sneakersImageView.image = UIImage(named: imageList[counter % 10])
+    }
+    
+    
+    @IBAction func nextButton(_ sender: Any) {
+        counter += 1
+        sneakersImageView.image = UIImage(named: imageList[counter % 10])
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
