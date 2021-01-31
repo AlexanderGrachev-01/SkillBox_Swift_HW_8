756c5f107c43dccaa8665c24fc94d360b907f2f4
Alexander Grachev <s89519011368@yandex.ru>
2021-01-31 19:08:41 +0300

    create func

--- HW_8/SecondViewController.swift
+++ HW_8/SecondViewController.swift
@@ -8,8 +8,28 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    func createProduct(_ x: CGFloat, _ y: CGFloat, _ imageName: String) {
+        let productImageView: UIImageView? = .init()
+    
+        productImageView?.image = UIImage(named: imageName)
+        view.addSubview(productImageView!)
+        productImageView?.frame = CGRect(x: x, y: y, width: 170, height: 250)
+        productImageView?.contentMode = .scaleAspectFit
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
+        
+        
+        
+//        firstImageView?.frame = CGRect(x: 20, y: 100, width: 170, height: 250)
+//        secondImageView?.frame = CGRect(x: 224, y: 100, width: 170, height: 250)
+//        thirdImageView?.frame = CGRect(x: 20, y: 450, width: 170, height: 250)
+//        fourthImageView?.frame = CGRect(x: 224, y: 450, width: 170, height: 250)
+        
     }
 }
