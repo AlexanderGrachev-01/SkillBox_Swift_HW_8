7863dc3d98cbbba0440723af50848f1b4729150f
Alexander Grachev <s89519011368@yandex.ru>
2021-01-31 20:51:58 +0300

    Add task 3

---
+++ HW_8/ThirdViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ThirdViewController.swift
+//  HW_8
+//
+//  Created by Александр Грачев on 31.01.2021.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    @IBOutlet weak var greenView: UIView!
+    
+    @IBOutlet weak var blueView: UIView!
+    
+    @IBOutlet weak var purpleView: UIView!
+    
+    var menuArray = ["First", "Second", "Third"]
+    var myFirstSegmentedControl = UISegmentedControl()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        blueView.isHidden = true
+        greenView.isHidden = true
+        purpleView.isHidden = true
+        
+        // create SegmentedContoller
+        myFirstSegmentedControl = UISegmentedControl(items: menuArray)
+        myFirstSegmentedControl.frame = CGRect(x: 107, y: 100, width: 200, height: 32)
+        view.addSubview(myFirstSegmentedControl)
+        
+        func selectedValue (target: UISegmentedControl) {
+            if target == self.myFirstSegmentedControl {
+                let segmentedIndex = target.selectedSegmentIndex
+                
+                if segmentedIndex == 0 {
+                    blueView.isHidden = false
+                    greenView.isHidden = true
+                    purpleView.isHidden = true
+                }
+                else if segmentedIndex == 1 {
+                    blueView.isHidden = true
+                    greenView.isHidden = false
+                    purpleView.isHidden = true
+                }
+                else {
+                    blueView.isHidden = true
+                    greenView.isHidden = true
+                    purpleView.isHidden = false
+                }
+            }
+        }
+        
+        myFirstSegmentedControl.addTarget(self, action: #selector(selectedValue), for: .valueChanged)
+    }
+}
