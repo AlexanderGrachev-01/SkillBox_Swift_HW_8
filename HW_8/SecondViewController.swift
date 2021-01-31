2712202e889cfb8c12c002c9500016a1110735f5
Alexander Grachev <s89519011368@yandex.ru>
2021-01-31 19:38:57 +0300

    Task 2 done

--- HW_8/SecondViewController.swift
+++ HW_8/SecondViewController.swift
@@ -11,25 +11,34 @@ class SecondViewController: UIViewController {
     
     func createProduct(_ x: CGFloat, _ y: CGFloat, _ imageName: String) {
         let productImageView: UIImageView? = .init()
+        let namePriceLabel: UILabel? = .init()
     
-        productImageView?.image = UIImage(named: imageName)
         view.addSubview(productImageView!)
+        view.addSubview(namePriceLabel!)
+        productImageView?.image = UIImage(named: imageName)
         productImageView?.frame = CGRect(x: x, y: y, width: 170, height: 250)
         productImageView?.contentMode = .scaleAspectFit
+        namePriceLabel?.numberOfLines = 2
+        namePriceLabel?.text = "Кроссовки Nike Air"
+        namePriceLabel?.frame = CGRect(x: x, y: y + 250 , width: 170, height: 21)
     }
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-        
-        
-        
-//        firstImageView?.frame = CGRect(x: 20, y: 100, width: 170, height: 250)
-//        secondImageView?.frame = CGRect(x: 224, y: 100, width: 170, height: 250)
-//        thirdImageView?.frame = CGRect(x: 20, y: 450, width: 170, height: 250)
-//        fourthImageView?.frame = CGRect(x: 224, y: 450, width: 170, height: 250)
-        
+        let nameArr = ["sneakers_1", "sneakers_2", "sneakers_3", "sneakers_4"]
+        var x: CGFloat = 20
+        var y: CGFloat = 100
+        var i = 0
+        for _ in 1...2 {
+            createProduct(x, y, nameArr[i])
+            i += 1
+            x += 204
+            createProduct(x, y, nameArr[i])
+            i += 1
+            x = 20
+            y += 350
+        }
     }
 }
